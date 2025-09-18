f555b83d67fa569231a0a675444fb6a86f81f9a0
Emerson2310 <emerson-fx@hotmail.com>
2025-09-18 19:50:30 -0300

    Commit do escopo inicial

---
+++ src/test/java/com/vrh/RH/RhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrh.RH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
